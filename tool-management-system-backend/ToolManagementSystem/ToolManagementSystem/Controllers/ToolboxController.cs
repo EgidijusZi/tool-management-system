78ddf3b23278ff9f1b24812ef4382e0984005518
Egidijus Zvicevičius <zvicevicius.egidijus@gmail.com>
2023-01-02 18:02:18 +0200

    Added toolbox controller

---
+++ tool-management-system-backend/ToolManagementSystem/ToolManagementSystem/Controllers/ToolboxController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToolManagementSystem.Core.Interfaces;
+
+namespace ToolManagementSystem.Api.Controllers
+{
+    public class ToolboxController : BaseController
+    {
+        private readonly IToolboxService _toolboxService;
+        public ToolboxController(IToolboxService toolboxService)
+        {
+            _toolboxService = toolboxService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var toolboxes = _toolboxService.GetAll();
+            return Ok(toolboxes);
+        }
+    }
+}
