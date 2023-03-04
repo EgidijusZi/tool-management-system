059a5be47d7509da34004c8b3fdbc7c8aaf74136
EgidijusZi <zvicevicius.egidijus@gmail.com>
2023-03-04 18:28:43 +0200

    added engines table

---
+++ tool-management-system-backend/ToolManagementSystem/ToolManagementSystem.Domain/Entities/Engine.cs
@@ -0,0 +1,10 @@
+﻿namespace ToolManagementSystem.Domain.Entities
+{
+    public class Engine
+    {
+        public Guid Id { get; set; }
+        public string EngineType { get; set; }
+        public int CSN { get; set; }
+        public int TSN { get; set; }
+    }
+}
